131280e9edddf43c2c005d41abd6e106c6a8d062
luiso-o <cheportillo@gmail.com>
2023-12-08 19:55:44 +0100

    primeros pasos

---
+++ 00-tiposDeDatos.py
@@ -0,0 +1,22 @@
+# mi primer hola mundo en Python
+print("Hola Mundo")
+print('Este también es un hola mundo en python :)')
+
+"""
+también puede comentar
+varias lineas usando tres
+comillas al inicio y al final
+"""
+
+
+'''
+O si lo prefieres puedes usar
+tres comillas simples :)
+'''
+
+# type sirve para imprimir la clase de dato que estás usando
+print(type("con type se imprimira la clase de dato que soy :)"))
+print(type(3))
+print(type(2.1))
+print(type(2 + 3j))
+print(type(True))
